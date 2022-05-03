49b31bf044ee1626638668dc7ac76b69b86f45bf
WindChenx <190235290@qq.com>
2022-05-03 17:53:59 +0800

    拦截器

--- src/main/java/com/wind/community/controller/IndexController.java
+++ src/main/java/com/wind/community/controller/IndexController.java
@@ -1,21 +1,13 @@
 package com.wind.community.controller;
 
 import com.wind.community.dao.PaginationDTO;
-import com.wind.community.dao.QuestionDTO;
 import com.wind.community.mapper.UserMapper;
-import com.wind.community.model.User;
 import com.wind.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class IndexController {
@@ -26,22 +18,8 @@ public class IndexController {
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model, @RequestParam(name = "page", defaultValue = "1") Integer page,
+    public String index(Model model, @RequestParam(name = "page", defaultValue = "1") Integer page,
                         @RequestParam(name = "size", defaultValue = "5") Integer size) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length > 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
         PaginationDTO pagination = questionService.list(page, size);
         model.addAttribute("pagination", pagination);
         return "index";
